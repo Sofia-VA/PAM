e66afff601bc4793b23d72ef74063338ffd1538f
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-03 13:32:57 -0500

    Estructura de app finalizada

--- tarea1/lib/home_page.dart
+++ tarea1/lib/home_page.dart
@@ -16,26 +16,34 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: const Text('Mc Flutter'),
         ),
-        body: Column(
-          children: [
-            Row(children: [
-              Icon(Icons.account_circle, size: 42),
-              Column(children: [
-                Text("Flutter McFlutter", style: TextStyle(fontSize: 20)),
-                Text("Experienced App Developer")
-              ])
-            ]),
-            Row(children: [
-              Text("123 Main Street"),
-              Text("(415) 555-0198"),
-            ]),
-            Row(children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.accessibility_new)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.timer)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.phone_android)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.phone_iphone)),
-            ])
-          ],
+        body: Container(
+          margin: const EdgeInsets.all(15),
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 3),
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Icon(Icons.account_circle, size: 42),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text("Flutter McFlutter", style: TextStyle(fontSize: 20)),
+                  Text("Experienced App Developer")
+                ])
+              ]),
+              SizedBox(height: 10),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text("123 Main Street"),
+                Text("(415) 555-0198"),
+              ]),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.accessibility_new)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.timer)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.phone_android)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.phone_iphone)),
+              ]),
+            ],
+          ),
         ));
   }
 }
