f957d9f52fc2ddfd0100d3b4bea01c985535e3e2
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-03 11:51:37 -0500

    Estructura básica de app

--- tarea1/lib/home_page.dart
+++ tarea1/lib/home_page.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Mc Flutter'),
+        ),
+        body: Column(
+          children: [
+            Row(children: [
+              Icon(Icons.account_circle, size: 42),
+              Column(children: [
+                Text("Flutter McFlutter", style: TextStyle(fontSize: 20)),
+                Text("Experienced App Developer")
+              ])
+            ]),
+            Row(children: [
+              Text("123 Main Street"),
+              Text("(415) 555-0198"),
+            ]),
+            Row(children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.accessibility_new)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.timer)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.phone_android)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.phone_iphone)),
+            ])
+          ],
+        ));
   }
 }
