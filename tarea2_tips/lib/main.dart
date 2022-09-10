0cf74e2e6885e3b52666e2e4a97f9e8a0fd6da34
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-10 12:34:43 -0500

    Error en actualización de commit pasado

--- tarea2_tips/lib/main.dart
+++ tarea2_tips/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       home: HomePage(),
     );
