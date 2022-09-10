b3494fc34c15628fb267a885b098a8169a016336
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-10 12:33:38 -0500

    Color principal de la aplicación es ahora verde

--- tarea2_tips/lib/main.dart
+++ tarea2_tips/lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+      ),
       home: HomePage(),
     );
   }
