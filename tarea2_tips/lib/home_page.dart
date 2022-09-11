b4b3c0f640c7b514965f81eb704cb04d8b63d5e4
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-10 19:05:18 -0500

    Tarea 2 terminada, cálculo de tips + alertas

--- tarea2_tips/lib/home_page.dart
+++ tarea2_tips/lib/home_page.dart
@@ -9,9 +9,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var serviceCostController = TextEditingController();
+  double tip = 0.0;
   int? currentRadio;
   bool roundUp = false;
   var radioGroup = {0: 'Amazing (20%)', 1: 'Good (18%)', 2: 'Okay (15%)'};
+  var radioMults = {0: 0.20, 1: 0.18, 2: 0.15};
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +30,7 @@ class _HomePageState extends State<HomePage> {
             title: Padding(
               padding: EdgeInsets.only(right: 24),
               child: TextField(
+                controller: serviceCostController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   label: Text("Cost of Service"),
@@ -50,7 +54,7 @@ class _HomePageState extends State<HomePage> {
           ),
           ListTile(
             leading: Icon(Icons.north_east),
-            title: Text("Round up tip"),
+            title: Text("Round up tip?"),
             trailing: Switch(
                 value: roundUp,
                 onChanged: (bool selected) {
@@ -65,12 +69,14 @@ class _HomePageState extends State<HomePage> {
               children: [
                 MaterialButton(
                   child: Text("CALCULATE"),
-                  onPressed: () {},
+                  onPressed: () {
+                    _tipCalculation();
+                  },
                   color: Colors.green,
                   textColor: Colors.white,
                 ),
                 Text(
-                  "Tip amount: \$20.00",
+                  "Tip amount: \$${tip}",
                   textAlign: TextAlign.right,
                 ),
               ],
@@ -79,10 +85,6 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
-  }
-
-  void _tipCalculation() {
-    // TODO: completar
   }
 
   radioGroupGenerator() {
@@ -101,5 +103,53 @@ class _HomePageState extends State<HomePage> {
           ),
         )
         .toList();
+  }
+
+  void _tipCalculation() {
+    if (serviceCostController.text.isEmpty) _emptyServiceCost();
+    if (currentRadio == null) _emptyRadio();
+    double serviceCost = double.parse(serviceCostController.text);
+    // Según el radio seleccionado, multiplicamos por el porcentaje
+    tip = serviceCost * radioMults[currentRadio]!;
+    // Checamos si se desea redondear, si no, redondeamos a 2 decimales
+    tip =
+        (roundUp) ? tip.ceilToDouble() : double.parse((tip).toStringAsFixed(2));
+    setState(() {});
+  }
+
+  Future<dynamic> _emptyServiceCost() {
+    return showDialog(
+        context: context,
+        builder: ((context) {
+          return AlertDialog(
+              title: Text("Error"),
+              content: Text(
+                  "Service cost empty!\nPlease type cost to calculate tip."),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Got it"))
+              ]);
+        }));
+  }
+
+  Future<dynamic> _emptyRadio() {
+    return showDialog(
+        context: context,
+        builder: ((context) {
+          return AlertDialog(
+              title: Text("Error"),
+              content: Text(
+                  "No tip percentage selected!\nPlease select to calculate tip."),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Got it"))
+              ]);
+        }));
   }
 }
