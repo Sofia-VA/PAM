93e382e6e1f6a6e88c5a86ebe92d3f34ebef4e17
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-30 21:32:48 -0500

    Layout de página home

---
+++ music_finder/lib/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          SizedBox(height: 50),
+          Center(
+              child:
+                  Text("Toque para escuchar", style: TextStyle(fontSize: 20))),
+          SizedBox(height: 50),
+          Expanded(
+            child: Column(mainAxisSize: MainAxisSize.max, children: [
+              AvatarGlow(
+                endRadius: 170,
+                child: Material(
+                    shape: CircleBorder(),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: InkWell(
+                        onTap: () {},
+                        splashColor: Color.fromARGB(109, 54, 0, 96),
+                        child: Ink.image(
+                            fit: BoxFit.scaleDown,
+                            height: 200,
+                            image: AssetImage(
+                                'assets/images/sound_waves_round.png')))), // image
+                // circleAvatar
+                // ClipRRect
+                // Material
+              ),
+            ]),
+          ), // avatarglow
+          MaterialButton(
+            onPressed: () {},
+            shape: CircleBorder(),
+            child: CircleAvatar(
+              child: Icon(Icons.favorite, color: Colors.black),
+              backgroundColor: Colors.white,
+            ),
+          ),
+          SizedBox(height: 230),
+        ],
+      )),
+    );
+  }
+}
